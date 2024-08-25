2c765aa69d858141e45c5cc186d6608a1ccae829
ahmed <ahmedboabae2@gmail.com>
2024-08-25 02:10:39 +0000

    mvp ready

---
+++ app/api/chat/route.js
@@ -0,0 +1,109 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { NextResponse } from "next/server";
+
+const systemPrompt = `
+You are an AI assistant designed to help students find and evaluate university professors. Your knowledge base includes detailed, reviews, rating, about, achievements, email, office hours, soft skills, teaching style, title, how often they quiz their students and subject areas for professors.
+
+When a user seeks professor recommendations, your task is to provide the relevant options based on their query. Utilize a Retrieval Augmented Generation (RAG) approach: first, retrieve the most relevant professor information from your database, then generate a concise response highlighting the top professors.
+
+Your responses should be clear, informative, and tailored to the user's specific needs. If necessary, ask clarifying questions to better understand the user's preferences and requirements.
+
+**Example queries you might receive:**
+- "Can you recommend a professor for an introductory computer science course?"
+- "I need a highly rated Data Structure professor known for engaging lectures. Any suggestions?"
+- "I am struggling in complex problems and need a professor who offers extra support. Who would you recommend?"
+- "What are some top-rated professors in Computer Science, considering their engaging lectures and strong teaching style?"
+
+Student can also ask question regarding professor name, office Hours, email, behavior and teaching styles.
+If someone ask irrelevant questions send these examples and ask them to ask like this.
+
+For each query, respond in this format:
+A suitable recommendation about the response of what have been asked:
+\n
+- **[Professor name]**
+  \t- [Brief 1-2 sentence description of why this professor is a good fit]
+
+Similar for other professors
+Feel free to ask for more details or additional criteria to refine the recommendations further.`;
+
+export async function POST(req) {
+  const data = await req.json();
+
+  const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  const index = pc.index("rag").namespace("ns1");
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+  const text = data[data.length - 1].content;
+  const model = genAI.getGenerativeModel({
+    model: "text-embedding-004",
+  });
+
+  const embedding = await model.embedContent(text);
+
+  const results = await index.query({
+    topK: 5,
+    includeMetadata: true,
+    vector: embedding.embedding.values,
+  });
+
+  let resultString = "";
+  results.matches.forEach((match) => {
+    resultString += `
+    Returned Results:
+    Professor: ${match.id}
+    Review: ${match.metadata.review}
+    Subject: ${match.metadata.subjects}
+    Ratings: ${match.metadata.ratings}
+    About: ${match.metadata.about}
+    Achievements: ${match.metadata.achievements}
+    Email: ${match.metadata.email}
+    Experience: ${match.metadata.experience}
+    Office Hours: ${match.metadata.officeHours}
+    Soft Skills: ${match.metadata.softSkills}
+    Teaching Style: ${match.metadata.teachingStyle}
+    Title: ${match.metadata.title}
+    \n\n`;
+  });
+
+  const lastMessage = data[data.length - 1];
+  const lastMessageContent = lastMessage.content + resultString;
+  const lastDataWithoutLastMessage = data.slice(0, data.length - 1);
+
+  const dota = await genAI.getGenerativeModel({
+    model: "gemini-1.5-pro",
+    systemInstruction: systemPrompt,
+  });
+
+  const request = [
+    systemPrompt,
+    ...lastDataWithoutLastMessage.map((msg) => msg.content),
+    lastMessageContent,
+  ];
+
+  const completionStream = await dota.generateContentStream(request, {
+    stream: true,
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of completionStream.stream) {
+          const content = chunk.candidates[0].content.parts[0].text;
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
+        }
+      } catch (err) {
+        controller.error(err);
+      } finally {
+        controller.close();
+      }
+    },
+  });
+  return new NextResponse(stream);
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,184 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useRef, useEffect } from "react";
+import {
+  Box,
+  Container,
+  IconButton,
+  InputBase,
+  Paper,
+  Stack,
+  ThemeProvider,
+  createTheme,
+  Typography,
+  AppBar,
+  Toolbar,
+  CircularProgress,
+} from "@mui/material";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Send, SmartToyOutlined } from "@mui/icons-material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5",
+    },
+    background: {
+      default: "#f5f5f5",
+    },
+  },
+});
 
 export default function Home() {
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content: `Hi! I'm the Rate My Professor support assistant. How can I help you today?`,
+    },
+  ]);
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(scrollToBottom, [messages]);
+
+  const sendMessage = async () => {
+    if (message === "") return;
+    setIsLoading(true);
+    setMessages((messages) => [
+      ...messages,
+      { role: "user", content: message },
+      { role: "assistant", content: "" },
+    ]);
+    setMessage("");
+
+    const response = fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([...messages, { role: "user", content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+      let result = "";
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          setIsLoading(false);
+          return result;
+        }
+        const text = decoder.decode(value || new Uint8Array(), {
+          stream: true,
+        });
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1];
+          let otherMessages = messages.slice(0, messages.length - 1);
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ];
+        });
+        return reader.read().then(processText);
+      });
+    });
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Box sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              ProFinder
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          sx={{
+            flexGrow: 1,
+            overflow: "auto",
+            backgroundColor: "background.default",
+            padding: 2,
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          <Container maxWidth="sm">
+            <Stack direction="column" spacing={2}>
+              {messages.map((message, index) => (
+                <Box
+                  key={index}
+                  sx={{
+                    display: "flex",
+                    justifyContent:
+                      message.role === "assistant" ? "flex-start" : "flex-end",
+                    width: "100%",
+                  }}
+                >
+                  <Paper
+                    elevation={2}
+                    sx={{
+                      p: "12px 15px",
+                      borderRadius: 3,
+                      backgroundColor:
+                        message.role === "assistant" ? "#ffffff" : "#e3f2fd",
+                      maxWidth: "80%",
+                      padding: "25px",
+                    }}
+                  >
+                    {message.role === "assistant" ? (
+                      <Box sx={{ lineHeight: "30px" }}>
+                        <Markdown remarkPlugins={[remarkGfm]}>
+                          {message.content}
+                        </Markdown>
+                      </Box>
+                    ) : (
+                      <Box sx={{ fontSize: "16px" }}>{message.content}</Box>
+                    )}
+                  </Paper>
+                </Box>
+              ))}
+              {isLoading && (
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <CircularProgress />
+                </Box>
+              )}
+              <div ref={messagesEndRef} />
+            </Stack>
+          </Container>
+        </Box>
+        <Paper
+          component="form"
+          sx={{
+            p: "4px",
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            backgroundColor: "#ffffff",
+            boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.1)",
+          }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            sendMessage();
+          }}
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <InputBase
+            sx={{
+              ml: 1,
+              flex: 1,
+              fontSize: "16px",
+            }}
+            placeholder="Type your message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <IconButton type="submit" sx={{ p: "10px", color: "primary.main" }}>
+            <Send />
+          </IconButton>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
